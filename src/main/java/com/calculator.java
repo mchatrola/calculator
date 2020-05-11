dac672dea376ab96e3df6d4d675e63fbf4369f78
chandramouli <Kondamudi.Chandramouli@iiitb.org>
2020-05-11 18:17:39 +0530

    "this is First commit"

---
+++ src/main/java/com/calculator.java
@@ -0,0 +1,21 @@
+package com;
+
+public class calculator {
+    public static void main(String[] args){
+        System.out.println("4 + 8 = " + add(6, 3));
+        System.out.println("14 - 8 = " + subtract(6,3));
+        System.out.println("4 * 9 = " + multiply(6,3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static long multiply(int a, int b) {
+        return a * b;
+    }
+}
