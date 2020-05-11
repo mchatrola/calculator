46bb5f006d0b96547cd84c8712bdd7bd0e07d7ba
chandramouli <Kondamudi.Chandramouli@iiitb.org>
2020-05-11 18:21:53 +0530

    "this is First commit"

--- src/main/java/com/calculator.java
+++ src/main/java/com/calculator.java
@@ -3,7 +3,7 @@ package com;
 public class calculator {
     public static void main(String[] args){
         System.out.println("4 + 8 = " + add(6, 3));
-        System.out.println("14 - 8 = " + subtract(6,3));
+        System.out.println("24 - 8 = " + subtract(6,3));
         System.out.println("4 * 9 = " + multiply(6,3));
     }
 
